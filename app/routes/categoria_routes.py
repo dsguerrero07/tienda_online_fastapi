e7f41446f69c1b0ae8f2378b92b2700725c7ebeb
dsguerrero07 <dsguerrero07@ucatolica.edu.co>
2025-10-28 23:07:24 -0500

    feat: implementa CRUD completo para categorías con validaciones

--- app/routes/categoria_routes.py
+++ app/routes/categoria_routes.py
@@ -1,7 +1,62 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException , status
+from sqlmodel import Session, select
+from app.db import engine
+from app.models.categoria import Categoria
 
 router = APIRouter(prefix="/categorias", tags=["Categorias"])
 
-@router.get("/")
+#crear categoria
+@router.post("/", response_model=Categoria, status_code=status.HTTP_201_CREATED)
+def crear_categoria(categoria: Categoria):
+    with Session(engine) as session:
+        #validar nombre unico
+        existente = session.exec(select(Categoria).where(Categoria.nombre == categoria.nombre)).first()
+        if existente:
+            raise HTTPException(status_code=400, detail="El nombre de la categoría ya existe")
+        
+        session.add(categoria)
+        session.commit()
+        session.refresh(categoria)
+        return categoria
+
+#listar categorias
+@router.get("/",status_code=status.HTTP_201_CREATED)
 def listar_categorias():
-    return {"mensaje": "Aquí se listarán las categorías"}
+    with Session(engine) as session:
+        categorias=session.exec(select(Categoria).where(Categoria.activa==True)).all()
+        return categorias
+
+#obtener categoria con su productos
+@router.get("/{categoria_id}")
+def obtener_categoria(categoria_id: int):
+    with Session(engine) as session:
+        categoria = session.get(Categoria, categoria_id)
+        if not categoria:
+            raise HTTPException(status_code=404, detail="Categoría no encontrada")
+        return categoria
+    
+#actualizar categoria
+@router.put("/{categoria_id}")
+def actualizar_categoria(categoria_id: int, datos: Categoria):
+    with Session(engine) as session:
+        categoria = session.get(Categoria, categoria_id)
+        if not categoria:
+            raise HTTPException(status_code=404, detail="Categoría no encontrada")
+        
+        categoria.nombre = datos.nombre
+        categoria.descripcion = datos.descripcion
+        session.commit()
+        session.refresh(categoria)
+        return categoria
+
+# Desactivar categoría
+@router.delete("/{categoria_id}")
+def desactivar_categoria(categoria_id: int):
+    with Session(engine) as session:
+        categoria = session.get(Categoria, categoria_id)
+        if not categoria:
+            raise HTTPException(status_code=404, detail="Categoría no encontrada")
+
+        categoria.activa = False
+        session.commit()
+        return {"mensaje": "Categoría desactivada correctamente"}
